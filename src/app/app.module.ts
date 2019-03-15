31cd032567e8ec7a1521a62017ae4cfdb3155726
Alex D'Amico <AlexDamico@users.noreply.github.com>
2019-03-15 15:38:19 -0400

    refactor(bootstrap): re-install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
