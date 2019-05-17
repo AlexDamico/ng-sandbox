a33129b0a286c2cc1dc693167ea84af0b4b74a9d
Alex D'Amico <AlexDamico@users.noreply.github.com>
2019-05-17 16:53:14 -0400

    feat(truncate): add text truncate mixins
    Single line and multi-line (ellipses, fade down, and fade out)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { MarqueeComponent } from './castles/marquee/marquee.component';
+import { TruncateComponent } from './castles/truncate/truncate.component';
 
 
 // Route components
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'marquee', component: MarqueeComponent }
+  { path: 'marquee', component: MarqueeComponent },
+  { path: 'truncate', component: TruncateComponent }
 ];
 
 @NgModule({
