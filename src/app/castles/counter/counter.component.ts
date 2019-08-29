47ce4293b5b16d7fb7d02d887e62d98f79fdc9fb
Alex D'Amico <AlexDamico@users.noreply.github.com>
2019-08-29 11:34:34 -0400

    feat(counter): add basic +/- function

--- src/app/castles/counter/counter.component.ts
+++ src/app/castles/counter/counter.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
+  count:number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  decrement() {
+  	this.count -= 1;
+  }
+
+  increment() {
+  	this.count += 1;
+  }
 }
